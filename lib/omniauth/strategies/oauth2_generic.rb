144a8c8d463406c1450ea1c3aaedf4fe4c8ad0a7
Matthias Hähnel <github.com@drhaehnel.de>
2020-03-05 12:42:09 +0100

    Update oauth2_generic.rb
    remove scope

--- lib/omniauth/strategies/oauth2_generic.rb
+++ lib/omniauth/strategies/oauth2_generic.rb
@@ -4,7 +4,7 @@ module OmniAuth
   module Strategies
     class OAuth2Generic < OmniAuth::Strategies::OAuth2
       option :name, 'oauth2_generic'
-      option :scope, 'email, profile, groups'
+      #option :scope, 'email, profile, groups'
       option :token_method, :post
 
       option :client_options, { # Defaults are set for GitLab example implementation
